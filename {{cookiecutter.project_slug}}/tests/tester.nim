bff769933d1126da15a89c1839307a4442506b20
Harshad Sharma <harshad@sharma.io>
2017-12-26 16:55:16 +0530

    Rename tests => tester. Add example tests.

---
+++ {{cookiecutter.project_slug}}/tests/tester.nim
@@ -0,0 +1,36 @@
+import unittest
+import osproc
+import sequtils
+import future
+import strutils
+
+const src_path = "./src/{{cookiecutter.project_slug}}.nim"
+const bin_path = "./bin/{{cookiecutter.project_slug}}"
+
+# Compile binaries
+doAssert execCmdEx("nim c -o:" & bin_path & " " & src_path).exitCode == QuitSuccess
+
+
+proc runCli(args: varargs[string]): tuple[output: string, exitCode: int] =
+  var quotedArgs = @args
+  quotedArgs.insert(bin_path)
+  quotedArgs = quoted_args.map((x: string) => ("\"" & x & "\""))
+  result = execCmdEx(quotedArgs.join(" "))
+  checkpoint(result.output)
+
+
+suite "Test cli":
+  let
+    default_output = "Hello, world."
+    test_name = "Tester"
+    test_output = "Hello, Tester."
+
+  test "Execute default":
+    let cmd_out = runCli()
+    check:
+      cmd_out.output.strip == default_output
+
+  test "Execute with --name":
+    let cmd_out = runCli("--name=" & test_name)
+    check:
+      cmd_out.output.strip == test_output
